48e56cebb14761d965d216ed55c16284482586df
aksharamuthu <paksharaaksharaa@gmail.com>
2025-04-29 19:13:38 +0530

    1st commit msg

---
+++ Oops.java
@@ -0,0 +1,37 @@
+package com.javaclass;
+
+public  class Oops {
+	
+	public  void studentName() {
+		System.out.println("Dhanush");
+	}
+	public void studentnumber() {
+		System.out.println(9944989698l);
+	}
+	public void studentid () {
+		System.out.println(281219);	
+	}
+	public void studentdegree () {
+		System.out.println("MBA");
+	}
+	public void studentheight() {
+		System.out.println(5.9f);		
+	}
+	public void studentlocation() {
+		System.out.println("tirunelveli");	
+	}
+	public void studentpassedout() {
+		System.out.println(2019);		
+	}
+	public static void main(String[] args) {
+		Oops s= new Oops();
+		s.studentName();
+		s.studentnumber();
+		s.studentid();
+		s.studentdegree();
+		s.studentheight();
+		s.studentlocation();
+		s.studentpassedout();
+	}
+	
+}
