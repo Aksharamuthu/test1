8d856ccd884501fa0038c557d0976dc3a3c35c5b
aksharamuthu <paksharaaksharaa@gmail.com>
2025-04-30 11:20:18 +0530

    1st commit msg

---
+++ Array.java
@@ -0,0 +1,34 @@
+package com.javaclass;
+
+public class Array {
+public static void main(String[] args) {
+	   int [] i=new int [5];
+	   
+	   
+	   i[0]=5;
+	   i[1]=10;
+	   i[2]=15;
+	   i[3]=20;
+	   i[4]=25;
+	   
+	   
+	   System.out.println(i[0]);
+	   System.out.println(i[1]);
+	   System.out.println(i[2]);
+       System.out.println(i[3]);
+	   System.out.println(i[4]);
+	   
+	   
+	  
+	 int j=0;
+	 do {
+		 System.out.println(i[j]);
+		 j++;
+		
+	}
+	 while (j<=4); 
+	 
+	}
+
+}
+
